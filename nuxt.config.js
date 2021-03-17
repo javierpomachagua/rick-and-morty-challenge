7c24a583aa346f6147609182bd5255f72f0e4391
Javier Pomachagua <javierpp0303@gmail.com>
2021-03-17 09:32:34 -0500

    [Fix] SSR

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'server',
-
+  ssr: 'false',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'rick-and-morty-challenge',
